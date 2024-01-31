514bb57446bc4f47c94d17757d8de33c41293dba
Christopher Richardson <christopher.richardson@gtri.gatech.edu>
2024-01-31 11:36:27 -0500

    group exercises by pattern similarity

---
+++ practice.py
@@ -0,0 +1,17 @@
+from tomshreds import *
+
+def get_practice_routine():
+    routine = []
+    routine.extend(drills_2s)
+    routine.extend(drills_2s_etudes)
+    routine.extend(drills_asc)
+    routine.extend(drills_desc)
+    routine.extend(drills_asc_desc)
+    routine.extend(drills_asc_desc_etudes)
+    routine.extend(drills_2sa)
+    routine.extend(drills_3sa)
+    routine.extend(drills_456sa)
+    routine.extend(drills_pt)
+    routine.extend(drills_cp)
+    routine.extend(drills_riff)
+    return routine
--- tomshreds.py
+++ tomshreds.py
@@ -1,5 +1,145 @@
 import random
 
+ss2s = {
+    "Single String On Beat": [
+        "Single String Ascending 1 (Chunk Only)",
+        "Single String Ascending 1 (Expanded)",
+        "Single String Descending 1 (Chunk Only)",
+        "Single String Descending 1 (Expanded)",
+        "Etude 1",
+    ],
+    "Single String Off Beat": [
+        "Single String Ascending 2 (Chunk Only)",
+        "Single String Ascending 2 (Expanded)",
+        "Single String Descending 2 (Chunk Only)",
+        "Single String Descending 2 (Expanded)",
+        "Etude 2",
+    ],
+    "REH Lick": [
+        "REH Lick (Chunk Only)",
+        "REH Lick (Expanded)",
+        "REH Lick w/ Pulloff (Chunk Only)",
+        "REH Lick w/ Pulloff (Expanded)",
+        "Etude 2",
+    ],
+    "Open String Licks": [
+        "Trilogy Open String Lick",
+        "Spellbound Open String Lick",
+        "Etude 5",
+        "Etude 6",
+    ],
+    "Economy Picking Two String Licks": [
+        "7th Sign Lick (Chunk Only)",
+        "7th Sign Lick (Expanded)",
+        "Youtube Circular Lick",
+        "Etude 3",
+        "Etude 4",
+    ],
+    "5 and 7 Note Licks": [
+        "Rising Force Ending Run (Chunk Only)",
+        "Rising Force Ending Run (Expanded)",
+        "Ascending 7s (Chunk Only)",
+        "Ascending 7s (Expanded)",
+        "Etude 7",
+        "Etude 8",
+    ],
+}
+
+asc_desc_456 = {
+    "Three Notes per String": [
+        "Ascending: Simple Run (Chunk Only)",
+        "Ascending: Simple Run (Expanded)",
+        "Ascending: Rising Force Melodic Minor Run",
+        "Descending: Simple",
+        "Ascending Descending: Simple",
+        "Ascending: Etude 1",
+        "Ascending: Etude 4",
+        "Descending: Etude 1",
+        "Descending: Etude 7",
+        "Ascending Descending: Etude 1",
+    ],
+    "Offset": [
+        "Ascending: Offset Run",
+        "Descending: Offset",
+        "Descending: Direction Switch",
+        "Descending: Viking Sequence",
+        "Ascending Descending: Offset",
+        "Ascending: Etude 2",
+        "Descending: Etude 2",
+        "Descending: Etude 3",
+        "Descending: Etude 4",
+        "Ascending Descending: Etude 2",
+    ],
+    "Eddy": [
+        "Ascending: Eddy Run",
+        "Ascending: Volcano Style Run",
+        "Descending: Trilogy Octave Repeating",
+        "Descending: Vertical Descending 4s",
+        "Descending: Vertical Descending 4s (Expanded)",
+        "Descending: Vertical Descending 4s (Locked Position)",
+        "Descending: Rising Force Ending Run Complete",
+        "Ascending: Etude 3"
+        "Descending: Etude 5",
+        "Descending: Etude 6",
+        "Descending: Etude 7",
+        "Ascending Descending: Etude 3",
+    ],
+    "E Root": [
+        "Ascending: E Root Run",
+        "Descending: E Root Simple",
+        "Descending: E Root + Simple String Run",
+        "Descending: E Root + Ascending Lead In",
+        "Descending: E Root Far Beyond",
+        "Ascending Descending: E Root",
+    ],
+}
+
+arpeggios = {
+    "B Root": [
+        "2 String: B String Root Part 1",
+        "2 String: B String Root Part 2",
+        "2 String: Diminished Inversions",
+        "2 String: Etude 1",
+        "3 String: B String Root (Part 1)",
+        "3 String: B String Root (Part 2)",
+        "3 String: B String Root (Part 3)",
+        "3 String: B String Root (Part 4)",
+        "3 String: Etude 2",
+        "4, 5, 6: 5 String Arpeggio Alternate Shape",
+    ],
+    "E Root": [
+        "2 String: E String Root",
+        "2 String: Etude 2",
+        "2 String: Etude 3",
+        "2 String: Etude 4",
+        "3 String: E String Root (Part 1)",
+        "3 String: E String Root (Part 2)",
+        "3 String: E String Root (Part 3)",
+        "Disciples Shapes (Part 1)",
+        "Disciples Shapes (Part 2)",
+        "3 String: Etude 3",
+        "3 String: Etude 4",
+        "3 String: Etude 5",
+        "4, 5, 6: 4 String Arpeggio (Badinerie)",
+        "4, 5, 6: Etude 2",
+    ],
+    "G Root": [
+        "3 String: G String Root (Part 1)",
+        "3 String: G String Root (Part 2)",
+        "3 String: G String Root (Part 3)",
+        "3 String: G String Root (Part 4)",
+        "3 String: Etude 1",
+        "3 String: Etude 6",
+        "4, 5, 6: 5 String Arpeggio Ascending",
+        "4, 5, 6: 5 String Arpeggio Descending",
+        "4, 5, 6: 5 String Arpeggio Complete",
+        "4, 5, 6: 6 String Arpeggio",
+        "4, 5, 6: Etude 1",
+    ],
+
+}
+
+
 drills_2s = [
     "Single String Ascending 1",
     "Single String Ascending 2",
@@ -11,6 +151,17 @@ drills_2s = [
     "Youtube Circular Lick",
     "Rising Force Ending Run",
     "Ascending 7s",
+]
+
+drills_2s_etudes = [
+    "Etude 1",
+    "Etude 2",
+    "Etude 3",
+    "Etude 4",
+    "Etude 5",
+    "Etude 6",
+    "Etude 7",
+    "Etude 8",
 ]
 
 drills_asc = [
@@ -41,6 +192,29 @@ drills_asc_desc = [
     "Simple",
     "Offset",
     "E Root",
+]
+
+drills_asc_desc_etudes = [
+    "Etude 1",
+    "Etude 2",
+    "Etude 3",
+]
+
+drills_bends = [
+    "Bends on the E String",
+    "Bends on the B String",
+    "Bends on the G String",
+    "Vibrato",
+    "Trills",
+    "Arpeggio Trills",
+    "Slide Shrieks",
+    "Etude 1",
+    "Etude 2",
+]
+
+drills_bends_etudes = [
+    "Etude 1",
+    "Etude 2",
 ]
 
 drills_2sa = [
@@ -104,9 +278,11 @@ drills_riff = [
 
 drills = {
     "2 String": drills_2s, 
+    "2 String Etudes": drills_2s_etudes, 
     "4, 5, & 6 String Ascending": drills_asc,
     "4, 5, & 6 String Descending": drills_desc, 
     "Ascending Descending": drills_asc_desc, 
+    "Ascending Descending Etudes": drills_asc_desc_etudes, 
     "2 String Arpeggios": drills_2sa, 
     "3 String Arpeggios": drills_3sa, 
     "4, 5, & 6 String Arpeggios": drills_456sa, 
